e5483309060eddfc075fc6507e5d702c5df80b37
sanketsud21 <sanketsud21@gmail.com>
2022-12-22 01:51:18 +0000

    adding prediction to webpage

--- app.py
+++ app.py
@@ -21,5 +21,15 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    print(data)
+    final_input=np.array(data).reshape(-1,1)
+    print(final_input)
+    output=regmodel.predict(final_input)
+    return render_template("home.html",prediction_text="The house Price prediction is {}".format(output[0]))
+
+
 if __name__=="__main__" :
     app.run(debug=True)
